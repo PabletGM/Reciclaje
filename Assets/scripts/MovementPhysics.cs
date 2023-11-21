0ec454fd6ced61e78afdcdd7196a541432ad391b
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-11-21 12:54:58 +0100

    intento ajustes movil
    -Arreglar tambien que al caer en zona a medias no detecta suelo y no deja saltar

--- Assets/scripts/MovementPhysics.cs
+++ Assets/scripts/MovementPhysics.cs
@@ -18,29 +18,42 @@ public class MovementPhysics : MonoBehaviour
     //    Debug.Log("Do something special here!");
     //}
 
-#region Ordenador
+    #region Ordenador
 
-//position where you press on screen
-private Vector3 mousePressDownPos;
-    //position where you release on screen
-    private Vector3 mouseReleasePos;
+        //position where you press on screen
+        private Vector3 mousePressDownPos;
+        //position where you release on screen
+        private Vector3 mouseReleasePos;
 
-    //force of the shoot
-    private float forceMultiplier =1f;
+        //force of the shoot
+        private float forceMultiplier =1f;
 
-    private float maxDistance = 80;
+        private float maxDistance = 80;
 
-    private Rigidbody rb;
+        private Rigidbody rb;
 
-    public float raycastDistance = 0.1f; // Distancia del rayo hacia abajo
-    public LayerMask groundLayer; // Capa que representa el suelo
-    private bool isGrounded;
-    private bool canShoot = false;
+        public float raycastDistance = 0.1f; // Distancia del rayo hacia abajo
+        public LayerMask groundLayer; // Capa que representa el suelo
+        private bool isGrounded;
+        private bool canShoot = false;
+
+        #endregion
 
 
+    #region Mobile
+        public float power = 10f;
+        public float maxDrag = 5f;
+
+        Vector3 dragStartPos;
+        Touch touch;
+
+
+
+        #endregion
 
     private void Update()
     {
+        #region Ordenador
         // Lanza un rayo hacia abajo desde la posición del jugador
         isGrounded = Physics.Raycast(transform.position, Vector3.down, raycastDistance, groundLayer);
 
@@ -51,8 +64,33 @@ private Vector3 mousePressDownPos;
         }
         else
         {
-            canShoot= false;
+            canShoot = false;
         }
+        #endregion
+
+        #region Movil
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if(Input.touchCount > 0)
+            {
+                //primer toque
+                touch = Input.GetTouch(0);
+                //empieza el toque
+                if(touch.phase == TouchPhase.Began)
+                {
+                    DragStart();
+                }
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    Dragging();
+                }
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    DragRelease();
+                }
+            }
+        }
+        #endregion
     }
 
     void Start()
@@ -60,33 +98,45 @@ private Vector3 mousePressDownPos;
         rb = GetComponent<Rigidbody>();
     }
 
+    #region Ordenador
+
     /// <summary>
     /// collects the position where you press on screen
     /// </summary>
     private void OnMouseDown()
     {
-
-        mousePressDownPos = Input.mousePosition;
-
+        #region Ordenador
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            mousePressDownPos = Input.mousePosition;
+        }
+        #endregion
     }
 
     //se llama cuando se acaba de hacer el drag
     private void OnMouseDrag()
     {
-        //fuerza inicial
+        #region Ordenador
+
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            //fuerza inicial
             Vector3 forceInit = (Input.mousePosition - mousePressDownPos);
             Vector3 forceV = (new Vector3(forceInit.x, forceInit.y, forceInit.y)) * forceMultiplier;
 
             float distance = Vector3.Distance(Input.mousePosition, mousePressDownPos) * forceMultiplier;
             //Debug.Log(distance);
-        if (distance <= maxDistance && canShoot)
-        {
-            DrawTrajectory.Instance.UpdateTrajectory(forceV, rb, Vector3.zero);
+            if (distance <= maxDistance && canShoot)
+            {
+                DrawTrajectory.Instance.UpdateTrajectory(forceV, rb, Vector3.zero);
+            }
+            else
+            {
+                DrawTrajectory.Instance.HideLine();
+            }
         }
-        else
-        {
-            DrawTrajectory.Instance.HideLine();
-        }
+        #endregion
+
     }
 
     /// <summary>
@@ -94,21 +144,26 @@ private Vector3 mousePressDownPos;
     /// </summary>
     private void OnMouseUp()
     {
-        DrawTrajectory.Instance.HideLine();
-        mouseReleasePos = Input.mousePosition;
-
-        //direction where the shot will go
-        Vector3 direction = mouseReleasePos - mousePressDownPos;
-        float distance = Vector3.Distance(mouseReleasePos, mousePressDownPos);
-        //para que tenga permiso debe detectar un raycast que está en el suelo
-        if(canShoot)
+        #region Ordenador
+        if (Application.platform == RuntimePlatform.WindowsEditor)
         {
-            Shoot(direction, distance);
+            DrawTrajectory.Instance.HideLine();
+            mouseReleasePos = Input.mousePosition;
+
+            //direction where the shot will go
+            Vector3 direction = mouseReleasePos - mousePressDownPos;
+            float distance = Vector3.Distance(mouseReleasePos, mousePressDownPos);
+            //para que tenga permiso debe detectar un raycast que está en el suelo
+            if (canShoot)
+            {
+                Shoot(direction, distance);
+            }
         }
-        
+        #endregion
+
     }
 
-    
+
 
     //shooting with force on Y axis
     void Shoot(Vector3 Force, float distance)
@@ -117,17 +172,53 @@ private Vector3 mousePressDownPos;
         //si quieres que sea misma fuerza todos los tiros
         //rb.AddForce(new Vector3(-Force.x, -Force.y,0).normalized * forceMultiplier);
         //si quieres que se pueda recargar y cuanto mas tirachinas mas fuerte
-
-        
-        //si supera limites de fuerza no tiramos
-        
-        if(distance <= maxDistance)
+        #region Ordenador
+        if (Application.platform == RuntimePlatform.WindowsEditor)
         {
-            rb.AddForce(new Vector3(-Force.x, -Force.y, 0) * forceMultiplier);
+            //si supera limites de fuerza no tiramos
+
+            if (distance <= maxDistance)
+            {
+                rb.AddForce(new Vector3(-Force.x, -Force.y, 0) * forceMultiplier);
+            }
         }
+        #endregion
     }
 
+    #endregion
 
+    #region Movil
+    private void DragStart()
+    {
+        dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
+        //dragStartPos.z = 0;
+    }
 
+    private void Dragging()
+    {
+        Vector3 draggingPos = Camera.main.ScreenToWorldPoint(touch.position);
+        //draggingPos.z = 0f;
+    }
+    private void DragRelease()
+    {
+        Vector3 dragReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
+        //draggingPos.z = 0f;
+
+        Vector3 force = dragStartPos - dragReleasePos;
+        Vector3 clampedforce = Vector3.ClampMagnitude(force, maxDrag) * power;
+
+        float distance = Vector3.Distance(dragStartPos, dragReleasePos) * power;
+        //Debug.Log(distance);
+        if (distance <= maxDistance && canShoot)
+        {
+            DrawTrajectory.Instance.UpdateTrajectory(clampedforce, rb, Vector3.zero);
+        }
+        else
+        {
+            DrawTrajectory.Instance.HideLine();
+        }
+
+        rb.AddForce(-clampedforce);
+    }
     #endregion
 }
